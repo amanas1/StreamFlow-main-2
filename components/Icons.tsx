c7779f4255f41c87e1b823ca77c2c1a7456f6ba8
amanas1 <61933757+amanas1@users.noreply.github.com>
2026-02-05 16:36:15 +0500

    feat: add share and link icons

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -328,3 +328,15 @@ export const ShuffleIcon: React.FC<{ className?: string }> = ({ className }) => 
   </svg>
 );
 
+export const ShareIcon: React.FC<{ className?: string }> = ({ className }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
+    </svg>
+);
+
+export const LinkIcon: React.FC<{ className?: string }> = ({ className }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.502 4.502 0 011.242 7.244l-4.5 4.5a4.502 4.502 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.502 4.502 0 00-6.364-6.364l-4.5 4.5a4.502 4.502 0 001.242 7.244" />
+    </svg>
+);
+
