fc0ddfaca7088beabbd4c3e059b0b363d6dd165e
amanas1 <61933757+amanas1@users.noreply.github.com>
2026-03-05 20:36:57 +0500

    Diagnostic SEO handler: minimal test

--- frontend/api/seo-handler.js
+++ frontend/api/seo-handler.js
@@ -1,5 +1,18 @@
 
 export default async function handler(req, res) {
-    res.setHeader('X-SEO-DEBUG', 'Minimal-Active');
-    return res.status(200).send('SEO Handler is reachable');
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("x-seo-handler", "running");
+
+  res.status(200).send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>SEO Handler Test</title>
+      <link rel="canonical" href="https://auradiochat.com/test-seo">
+    </head>
+    <body>
+      SEO HANDLER WORKING
+    </body>
+    </html>
+  `);
 }
