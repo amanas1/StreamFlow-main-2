9dfc64c198288c442e209e35d63e179afc877e2a
amanas1 <61933757+amanas1@users.noreply.github.com>
2026-03-05 20:38:51 +0500

    SEO handler debug: List files

--- frontend/api/seo-handler.js
+++ frontend/api/seo-handler.js
@@ -3,27 +3,12 @@ import fs from 'fs';
 import path from 'path';
 
 export default async function handler(req, res) {
-  const indexPath = path.join(process.cwd(), 'dist', 'index.html');
-  let html = '';
-  let error = null;
+  const info = {
+    cwd: process.cwd(),
+    dirname: __dirname,
+    rootFiles: fs.readdirSync(process.cwd()),
+  };
 
-  try {
-    if (fs.existsSync(indexPath)) {
-      html = fs.readFileSync(indexPath, 'utf8');
-    } else {
-      error = 'File not found at ' + indexPath;
-    }
-  } catch (e) {
-    error = e.message;
-  }
-
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("x-seo-handler", "step-2");
-  if (error) {
-    res.setHeader("x-seo-error", error);
-    return res.status(200).send(`Error: ${error}`);
-  }
-
-  res.setHeader("x-seo-file-size", html.length.toString());
-  return res.status(200).send(html);
+  res.setHeader("Content-Type", "application/json");
+  return res.status(200).json(info);
 }
